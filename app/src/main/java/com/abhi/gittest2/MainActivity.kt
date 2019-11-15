36e5c5218039814c9aa0329f7bd7a789b810b5cd
mac <rijo@appinessworld.com>
2019-11-15 18:13:39 +0530

    Rijo's commit

--- app/src/main/java/com/abhi/gittest2/MainActivity.kt
+++ app/src/main/java/com/abhi/gittest2/MainActivity.kt
@@ -8,9 +8,11 @@ class MainActivity : AppCompatActivity() {
 
     // this is Rijo's Edit
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        txv1.text="Rijo"
 
         txv1.text = "Gitest1"
     }
