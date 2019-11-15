617ccc07a5aa0bdbf0708efc33580f4e389c6159
mac <rijo@appinessworld.com>
2019-11-15 18:06:30 +0530

    Rijo's commit

--- app/src/main/java/com/abhi/gittest2/MainActivity.kt
+++ app/src/main/java/com/abhi/gittest2/MainActivity.kt
@@ -6,6 +6,8 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    // this is Rijo's Edit
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
